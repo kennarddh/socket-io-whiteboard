eb1b5526b0aeb432cd891e098ee694b0cbe755ee
kennarddh <kennarddh@gmail.com>
2021-11-01 12:04:56 +0700

    room list page, room page, disconnect event for room owner and client

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { disconnect, send } = require('process')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
@@ -14,7 +15,71 @@ server.listen(port, () => {
 
 app.use(express.static(__dirname + '/public'));
 
+const GenerateRandomString = (length) => {
+    let result = ''
+    const characters  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (let i = 0; i < length; i++) {
+        result += characters[(Math.floor(Math.random() * characters.length))]
+    }
+
+    return result;
+}
+
 io.on('connection', (socket) => {
+    users[socket.id] = {
+        room: '',
+        name: '',
+        score: 0
+    }
+
+    socket.on('disconnect', () => {
+        if (users[socket.id].room) {
+            io.to(users[socket.id].room).emit('exit_room')
+            
+            if (rooms[users[socket.id].room].owner == socket.id) {
+                
+                Object.keys(users).forEach((key) => {
+                    value = users[key]
+
+                    io.in(users[socket.id].room).socketsLeave(users[socket.id].room)
+                    
+                    if (value.room == users[socket.id].room) {
+                        users[key].room = ''
+                    }
+                }) 
+                
+                delete rooms[users[socket.id].room]
+
+                return
+            }
+
+            const sendData = {}
+    
+            Object.keys(users).forEach((key) => {
+                value = users[key]
+                if (key !== socket.id) {
+                    if (value.room == users[socket.id].room) {
+                        sendData[key] = value
+                    }
+                }
+            })
+            
+            if (Object.keys(sendData).length <= 0) {
+                delete rooms[users[socket.id].room]
+
+                return
+            }
+
+            io.to(users[socket.id].room).emit('recive_room_player_list', {
+                players: sendData,
+                room_owner: rooms[users[socket.id].room].owner
+            })
+        }
+
+        delete users[socket.id]
+    })
+
     // draw
     socket.on('clear', () => {
         io.sockets.emit('clear')
@@ -32,6 +97,97 @@ io.on('connection', (socket) => {
         io.sockets.emit('mouseup')
     })
 
-    // join room
-    
+    // room
+    socket.on('update_room_list', () => {
+        const sendRooms = {}
+
+        Object.keys(rooms).forEach((item) => {
+            if (rooms[item].status != 'close') {
+                sendRooms[item] = rooms[item] 
+            }
+        })
+
+        socket.emit('update_room_list', {
+            rooms: sendRooms
+        })
+    })
+
+    socket.on('joinRoom', (data) => {
+        if (!users[socket.id].rooms) {
+            if (users[socket.id].rooms != socket.id) {
+                socket.leave(users[socket.id].rooms)
+            }
+        }
+
+        socket.join(data.room_name)
+        
+        users[socket.id].room = data.room_name
+
+        if (data.player_name) {
+            users[socket.id].name = data.player_name
+        } else {
+            users[socket.id].name = GenerateRandomString(10)
+        }
+
+        console.log('emit successfully_join_room')
+
+        io.to(socket.id).emit('successfully_join_room', {
+            room_name: data.room_name,
+            room_owner: rooms[data.room_name].owner
+        })
+    })
+
+    socket.on('createRoom', (data) => {
+        if (!users[socket.id].rooms) {
+            if (users[socket.id].rooms != socket.id) {
+                socket.leave(users[socket.id].rooms)
+            }
+        }
+
+        users[socket.id].room = data.room_name
+        
+        if (data.player_name) {
+            users[socket.id].name = data.player_name
+        } else {
+            users[socket.id].name = GenerateRandomString(10)
+        }
+
+        socket.join(data.room_name)
+
+        rooms[data.room_name] = {
+            status: 'open',
+            owner: socket.id
+        }
+
+        io.emit('update_room_list', {
+            rooms: rooms
+        })
+        
+        console.log('emit successfully_join_room')
+
+        io.to(socket.id).emit('successfully_join_room', {
+            room_name: data.room_name,
+            room_owner: rooms[data.room_name].owner
+        })
+    })
+
+    // room page
+    socket.on('update_room_player_list', (data) => {
+        const sendData = {}
+
+        Object.keys(users).forEach((key) => {
+            value = users[key]
+            if (value.room == data.room_name) {
+                sendData[key] = value
+            }
+        })
+
+        io.to(data.room_name).emit('recive_room_player_list', {
+            players: sendData,
+            room_owner: rooms[data.room_name].owner
+        })
+    })
+    socket.onAny((event, ...arg) => {
+        console.log(event, arg)
+    })
 })
--- public/main.js
+++ public/main.js
@@ -1,3 +1,7 @@
+const roomListPage = document.querySelector('.room-list-page')
+const roomPage = document.querySelector('.room-page')
+const gamePage = document.querySelector('.game-page')
+
 const socket = io()
 const canvas = document.getElementById("paint-canvas")
 const context = canvas.getContext("2d")
@@ -135,3 +139,99 @@ const ToggleCanDraw = (bool) => {
         leftPanel.style.visibility = 'unset'
     }
 }
+
+// join room
+const playerNameInput = document.querySelector('.room-list-page #player-name')
+
+socket.emit('update_room_list')
+
+const roomListTableBody = document.querySelector('.room-list-page .room-list')
+
+socket.on('update_room_list', (data) => {
+    roomListTableBody.innerHTML = ''
+
+    Object.keys(data.rooms).forEach((key) => {
+        item = data.rooms[key]
+        
+        let list = document.createElement('tr')
+        let name = document.createElement('td')
+        let buttonTd = document.createElement('td')
+        let button = document.createElement('button')
+        button.innerHTML = 'Join'
+        name.innerHTML = key
+
+        button.addEventListener('click', () => {
+            socket.emit('joinRoom', {
+                room_name: key,
+                player_name: playerNameInput.value
+            })
+        })
+
+        buttonTd.appendChild(button)
+
+        list.appendChild(name)
+        list.appendChild(buttonTd)
+
+        roomListTableBody.appendChild(list)
+    })
+})
+
+const createRoomForm = document.querySelector('.room-list-page #create-room')
+
+createRoomForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    if (createRoomForm.querySelector('input').value) {
+        socket.emit('createRoom', {
+            room_name: createRoomForm.querySelector('input').value,
+            player_name: playerNameInput.value
+        })
+    }
+
+    createRoomForm.querySelector('input').value = ''
+})
+
+socket.on('successfully_join_room', (data) => {
+    console.log('received successfully_join_room')
+
+    roomListPage.style.display = 'none'
+    roomPage.style.display = 'block'
+
+    socket.emit('update_room_player_list', {
+        room_name: data.room_name
+    })
+})
+
+socket.onAny((event, ...arg) => {
+    console.log(event, arg)
+})
+
+const roomPlayerListTableBody = document.querySelector('.room-page .member-list tbody')
+const roomStartButton = document.querySelector('.room-page #start-button')
+
+socket.on('recive_room_player_list', (data) => {
+    roomPlayerListTableBody.innerHTML = ''
+
+    Object.keys(data.players).forEach((key) => {
+        item = data.players[key]
+        
+        let list = document.createElement('tr')
+        let name = document.createElement('td')
+        name.innerHTML = item.name
+
+        list.appendChild(name)
+
+        roomPlayerListTableBody.appendChild(list)
+    })
+
+    if (data.room_owner == socket.id) {
+        roomStartButton.style.display = 'block'
+    }
+})
+
+socket.on('exit_room', () => {
+    roomPage.style.display = 'none'
+    roomListPage.style.display = 'block'
+
+    socket.emit('update_room_list')
+})
